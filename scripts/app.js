f7504224858d0845af5a55b13e76cd93b38549ea
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-09-26 20:46:10 -0400

    fixing bugs

--- scripts/app.js
+++ scripts/app.js
@@ -2,45 +2,50 @@ const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
 const time = document.querySelector("img.time");
-const icon = document.querySelector("icon img");
+const icon = document.querySelector(".icon img");
 
 const updateUI = (data) => {
   // Retrieving information from the return of updateCity
-  // const cityDetails = data.cityDetails;
-  // const weatherDetails = data.weatherDetails;
+  // const cityDets = data.cityDets;
+  // const weather = data.weather;
 
   // Destructure Properties
-  const { cityDetails, weatherDetails } = data;
+  const { cityDets, weather } = data;
 
   details.innerHTML = `
-          <h5 class="my-3">${cityDetails.EnglishName}</h5>
-          <div class="my-3">${weatherDetails.WeatherText}</div>
+          <h5 class="my-3">${cityDets.EnglishName}</h5>
+          <div class="my-3">${weather.WeatherText}</div>
           <div class="display-4 my-4">
-            <span>${weatherDetails.Temperature.Metric.Value}</span>
+            <span>${weather.Temperature.Metric.Value}</span>
             <span>&deg;C</span>
           </div>
   `;
-};
-//Night Day Image and Icon
-let timeSrc = weatherDetails.isDayTime ? "../img/day.svg" : "../img/night"
 
-time.setAttribute("src", timeSrc);
+  const iconSrc = `../img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
 
-const iconSrc = `../img/icons/${weather.WeatherIcon}.svg`;
-icon.setAttribute("src", iconSrc);
+  let timeSrc = null;
+  if (weather.IsDayTime) {
+    timeSrc = "../img/day.svg";
+  } else {
+    timeSrc = "../img/night.svg";
+  }
 
-// Remove the d-none class if present
-if (card.classList.contains("d-none")) {
-  card.classList.remove("d-none");
-}
+  time.setAttribute("src", timeSrc);
 
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }}
+  // Night Day Image and Icon  
+  // let timeSrc = weather.isDayTime ? "../img/day.svg" : "../img/night.svg";
+  // Remove the d-none class if present
 const updateCity = async (city) => {
-  const cityDetails = await getCity(city);
-  const weatherDetails = await getWeather(cityDetails.Key);
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
 
   return {
-    cityDetails,
-    weatherDetails,
+    cityDets,
+    weather,
   };
 };
 
@@ -53,4 +58,4 @@ cityForm.addEventListener("submit", (event) => {
   updateCity(city)
     .then((data) => updateUI(data))
     .catch((err) => console.error(err));
-});
+})
